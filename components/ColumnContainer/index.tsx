278e5698040afd385e3d6730933f59b157988912
victornwaiwu <victor.nwaiwu@andela.com>
2019-08-02 01:50:59 +0100

    create component for rows and columns for grid system

---
+++ components/ColumnContainer/index.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+interface ColumnProps {
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+};
+
+const getWidthString = (value: number) => {
+  if (!value) return;
+
+  let size = (value / 12) * 100;
+
+  return `max-width: ${size}%; flex: 0 0 ${size}%;`;
+};
+
+const ColumnContainer = styled.div<ColumnProps>`
+  padding-left: 15px;
+  padding-right: 15px;
+  float: left;
+  margin-bottom: 10px;
+
+  ${({ xs }) => (xs ? getWidthString(xs) : "max-width: 100%; flex: 0 0 100%")};
+
+  @media only screen and (min-width: 600px) {
+    ${({ sm }) => sm && getWidthString(sm)};
+  }
+
+  @media only screen and (min-width: 900px) {
+    ${({ md }) => md && getWidthString(md)};
+  }
+
+  @media only screen and (min-width: 1200px) {
+    ${({ lg }) => lg && getWidthString(lg)};
+  }
+`;
+
+export default ColumnContainer;
---
+++ components/RowContainer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RowContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
+  clear: both;
+`;
+
+export default RowContainer;
