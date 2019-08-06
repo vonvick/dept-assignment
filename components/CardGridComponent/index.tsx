6e385aabf74265712aa44c83599f13d3915b0291
Victor Nwaiwu <victor.nwaiwu@andela.com>
2019-08-06 11:06:13 +0100

    create card grid component for displaying cases in grids

---
+++ components/CardGridComponent/index.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { CardProps } from '../CardComponent';
+
+// components
+import RowContainer from '../RowContainer';
+import ColumnContainer from '../ColumnContainer';
+import CardComponent from '../CardComponent';
+
+interface GridProps {
+  cases: CardProps[];
+}
+
+const StyledGridContainer = styled.div`
+  .other-cases {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const GridComponent = ({ cases }: GridProps) => {
+  const casesDisplay = (items: any) => {
+    return items.map((item: any, index: number) => {
+      const { imageName, client, title } = item;
+      return (
+        <ColumnContainer md={6} sm={12} key={index}>
+          <CardComponent
+            imageName={imageName}
+            client={client.toUpperCase()}
+            title={title}/>
+        </ColumnContainer>
+      );
+    });
+  };
+
+  const firstCasesList = casesDisplay(cases.slice(0, 4));
+  const secondCasesList = casesDisplay(cases.slice(5, 7));
+  const thirdCasesList = casesDisplay(cases.slice(7, 11));
+  const fourthCasesList = casesDisplay(cases.slice(11, 13));
+
+  return (
+    <div className="container">
+      <RowContainer>
+        {firstCasesList}
+      </RowContainer>
+      <RowContainer>
+        <ColumnContainer md={8} sm={12}>
+          <CardComponent
+            imageName={cases[4].imageName}
+            client={cases[4].client.toUpperCase()}
+            title={cases[4].title}/>
+        </ColumnContainer>
+        <ColumnContainer md={4} sm={12}>
+          <div className="other-cases">
+            <div className="other-case">
+
+            </div>
+          </div>
+        </ColumnContainer>
+      </RowContainer>
+      <RowContainer>
+        {secondCasesList}
+      </RowContainer>
+      <RowContainer>
+        {thirdCasesList}
+      </RowContainer>
+      <RowContainer>
+        {fourthCasesList}
+      </RowContainer>
+    </div>
+  );
+};
+
+export default GridComponent;
