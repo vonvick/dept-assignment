fc94b62884b1a64d03a3a7b8b348e13da5dcb7c4
Victor Nwaiwu <victor.nwaiwu@andela.com>
2019-08-07 11:33:27 +0100

    create filter component

---
+++ components/FilterComponent/index.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+const StyledFilter = styled.div`
+  margin: 100px 0 50px 0;
+  .filter-wrapper {
+    display: flex;
+    flex-direction: column;
+    @media screen and (min-width: 900px) {
+      justify-content: flex-end;
+      flex-direction: row;
+    }
+    .category-filter {
+      margin-right: 20px;
+    }
+  }
+`;
+
+const FilterComponent = () => {
+  return (
+    <StyledFilter>
+      <div className="container">
+        <div className="filter-wrapper">
+          <div className="category-filter">
+            <span className="category-text">Show me</span>
+            <span className="category-text"></span>
+          </div>
+          <div className="industry-filter">
+            <span className="category-text">in</span>
+            <span className="category-text"></span>
+          </div>
+        </div>
+      </div>
+    </StyledFilter>
+  );
+};
+
+export default FilterComponent;
